e57607f234f4a7c89c98dc20b611934faa406ddf
Miya Keilin <miya.keilin@gmail.com>
2020-07-16 11:19:21 -0700

    adding topic modelling files

---
+++ news_project/helper.py
@@ -0,0 +1,198 @@
+# for helper functions
+import pandas as pd
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
+import string
+import re
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+from nltk.tokenize import TweetTokenizer, RegexpTokenizer
+from gensim.models.coherencemodel import CoherenceModel
+import gensim
+
+# contraction dictionary
+c_dict = {
+    "ain't": "am not",
+    "aren't": "are not",
+    "can't": "cannot",
+    "can't've": "cannot have",
+    "'cause": "because",
+    "could've": "could have",
+    "couldn't": "could not",
+    "couldn't've": "could not have",
+    "didn't": "did not",
+    "doesn't": "does not",
+    "don't": "do not",
+    "hadn't": "had not",
+    "hadn't've": "had not have",
+    "hasn't": "has not",
+    "haven't": "have not",
+    "he'd": "he would",
+    "he'd've": "he would have",
+    "he'll": "he will",
+    "he'll've": "he will have",
+    "he's": "he is",
+    "how'd": "how did",
+    "how'd'y": "how do you",
+    "how'll": "how will",
+    "how's": "how is",
+    "i'd": "I would",
+    "i'd've": "I would have",
+    "i'll": "I will",
+    "i'll've": "I will have",
+    "i'm": "I am",
+    "i've": "I have",
+    "isn't": "is not",
+    "it'd": "it had",
+    "it'd've": "it would have",
+    "it'll": "it will",
+    "it'll've": "it will have",
+    "it's": "it is",
+    "let's": "let us",
+    "ma'am": "madam",
+    "mayn't": "may not",
+    "might've": "might have",
+    "mightn't": "might not",
+    "mightn't've": "might not have",
+    "must've": "must have",
+    "mustn't": "must not",
+    "mustn't've": "must not have",
+    "needn't": "need not",
+    "needn't've": "need not have",
+    "o'clock": "of the clock",
+    "oughtn't": "ought not",
+    "oughtn't've": "ought not have",
+    "shan't": "shall not",
+    "sha'n't": "shall not",
+    "shan't've": "shall not have",
+    "she'd": "she would",
+    "she'd've": "she would have",
+    "she'll": "she will",
+    "she'll've": "she will have",
+    "she's": "she is",
+    "should've": "should have",
+    "shouldn't": "should not",
+    "shouldn't've": "should not have",
+    "so've": "so have",
+    "so's": "so is",
+    "that'd": "that would",
+    "that'd've": "that would have",
+    "that's": "that is",
+    "there'd": "there had",
+    "there'd've": "there would have",
+    "there's": "there is",
+    "they'd": "they would",
+    "they'd've": "they would have",
+    "they'll": "they will",
+    "they'll've": "they will have",
+    "they're": "they are",
+    "they've": "they have",
+    "to've": "to have",
+    "wasn't": "was not",
+    "we'd": "we had",
+    "we'd've": "we would have",
+    "we'll": "we will",
+    "we'll've": "we will have",
+    "we're": "we are",
+    "we've": "we have",
+    "weren't": "were not",
+    "what'll": "what will",
+    "what'll've": "what will have",
+    "what're": "what are",
+    "what's": "what is",
+    "what've": "what have",
+    "when's": "when is",
+    "when've": "when have",
+    "where'd": "where did",
+    "where's": "where is",
+    "where've": "where have",
+    "who'll": "who will",
+    "who'll've": "who will have",
+    "who's": "who is",
+    "who've": "who have",
+    "why's": "why is",
+    "why've": "why have",
+    "will've": "will have",
+    "won't": "will not",
+    "won't've": "will not have",
+    "would've": "would have",
+    "wouldn't": "would not",
+    "wouldn't've": "would not have",
+    "y'all": "you all",
+    "y'alls": "you alls",
+    "y'all'd": "you all would",
+    "y'all'd've": "you all would have",
+    "y'all're": "you all are",
+    "y'all've": "you all have",
+    "you'd": "you had",
+    "you'd've": "you would have",
+    "you'll": "you you will",
+    "you'll've": "you you will have",
+    "you're": "you are",
+    "you've": "you have"
+}
+
+c_re = re.compile('(%s)' % '|'.join(c_dict.keys()))
+
+# stop words
+# add more, ex: news outlet name
+add_stop = ['said', 'say', '...', 'like', 'cnn', 'ad'] 
+stop_words = ENGLISH_STOP_WORDS.union(add_stop)
+
+punc = list(set(string.punctuation))
+
+def word_count(text):
+    return len(str(text).split(' '))
+
+def casual_tokenizer(text):
+    tokenizer = TweetTokenizer()
+    tokens = tokenizer.tokenize(text)
+    return tokens
+
+def expandContractions(text, c_re=c_re):
+    def replace(match):
+        return c_dict[match.group(0)]
+    return c_re.sub(replace, text)
+
+def process_text(text):
+    text = casual_tokenizer(text)
+    text = [each.lower() for each in text]
+    text = [re.sub('[0-9]+', '', each) for each in text]
+    text = [expandContractions(each, c_re=c_re) for each in text]
+    text = [SnowballStemmer('english').stem(each) for each in text]
+    text = [w for w in text if w not in punc]
+    text = [w for w in text if w not in stop_words]
+    text = [each for each in text if len(each) > 1]
+    text = [each for each in text if ' ' not in each]
+    return text
+
+def top_words(topic, n_top_words):
+    return topic.argsort()[:-n_top_words - 1:-1]  
+
+def topic_table(model, feature_names, n_top_words):
+    topics = {}
+    for topic_idx, topic in enumerate(model.components_):
+        t = (topic_idx)
+        topics[t] = [feature_names[i] for i in top_words(topic, n_top_words)]
+    return pd.DataFrame(topics)
+
+def whitespace_tokenizer(text): 
+    pattern = r"(?u)\b\w\w+\b" 
+    tokenizer_regex = RegexpTokenizer(pattern)
+    tokens = tokenizer_regex.tokenize(text)
+    return tokens
+
+def unique_words(text): 
+    ulist = []
+    [ulist.append(x) for x in text if x not in ulist]
+    return ulist
+
+def compute_coherence_values(corpus, dictionary, num, texts):
+    lda_model = gensim.models.wrappers.LdaMallet(
+        'mallet-2.0.8/bin/mallet', 
+        corpus=corpus, 
+        num_topics=num, 
+        id2word=dictionary,
+        workers=2
+    )
+    coherence_model_lda = CoherenceModel(model=lda_model, texts=texts, dictionary=dictionary, coherence='c_v')
+    return coherence_model_lda.get_coherence()
---
+++ news_project/nmf.py
@@ -0,0 +1,274 @@
+import pandas as pd
+import numpy as np
+from sklearn.decomposition import NMF
+from sklearn.feature_extraction.text import TfidfVectorizer
+from gensim.models.coherencemodel import CoherenceModel
+from gensim.corpora.dictionary import Dictionary
+from gensim.models.nmf import Nmf
+from collections import Counter
+from operator import itemgetter
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style('darkgrid')
+
+from helper import word_count, process_text, topic_table, whitespace_tokenizer, unique_words
+
+# parameters
+# dictionary
+no_below = 3 # words must appear in >=3 articles
+no_above = 0.85 # words can't appear in >85% of articles
+keep_n = 5000 # keep top 5000 of remaining words
+# gensim-coherence nmf
+chunksize = 2000
+passes = 5
+kappa = 0.1
+min_prob = 0.01
+w_max_iter = 300
+w_stop_condition = 0.0001
+h_max_iter = 100
+h_stop_condition = 0.001
+eval_every = True
+# tf-idf
+min_df = 3
+max_df = 0.85
+max_features = 5000
+# sklearn-model nmf
+max_iter = 500
+l1_ratio = 0.0
+alpha = 0.0
+tol = 0.0001
+
+path = '/Users/miya/Downloads/news_project/test_nmf_1/'
+test_path = 'test_nmf_1/'
+
+# read in data
+df = pd.read_csv('clean_shayna.csv',engine='python')
+
+# create new column 'word_count'
+df['word_count'] = df['text'].apply(word_count)
+
+# create new column 'processed_text'
+df['processed_text'] = df['text'].apply(process_text)
+
+# flatten list of lists
+p_text = df['processed_text']
+p_text = [item for sublist in p_text for item in sublist]
+
+# count frequency of word in all documents
+top_20 = pd.DataFrame(
+    Counter(p_text).most_common(20),
+    columns=['word','frequency']
+)
+num_unique_words = len(set(p_text))
+
+# create dictionary to pass as input to gensim model
+texts = df['processed_text']
+dictionary = Dictionary(texts)
+
+# filter out words 
+dictionary.filter_extremes(
+    no_below=no_below,
+    no_above=no_above,
+    keep_n=keep_n
+)
+
+# convert to bag of words (corpus)
+# need corpus to pass as input to gensim nmf model
+# [[(word_id, # times word appears in document),...],...]
+corpus = [dictionary.doc2bow(text) for text in texts]
+topic_nums = list(np.arange(5,75+1,5))
+
+# find optimal # of topics
+# use gensim nmf model so we can pass as input to gensim CoherenceModel
+coherence_scores = []
+for num in topic_nums:
+    # initialize NMF model
+    # gensim model
+    nmf = Nmf(
+        corpus=corpus,
+        num_topics=num,
+        id2word=dictionary, # mapping from word IDs to words
+        chunksize=chunksize, # number of documents to be used in each training chunk
+        passes=passes, # number of full passes over the training corpus
+        kappa=kappa, # gradient descent step size (larger => model trains faster but could lead to non-convergence if set too large)
+        minimum_probability=min_prob, # topics with smaller probabilities are filtered out
+        w_max_iter=w_max_iter, # max number of iterations to train W per each batch
+        w_stop_condition=w_stop_condition, # if error difference < value, training of W stops for the current batch
+        h_max_iter=h_max_iter, # max number of iterations to train h per each batch
+        h_stop_condition=h_stop_condition, # if error difference < value, training of H stops for the current batch
+        eval_every=eval_every, # number of batches after which l2 norm of (v - Wh) is computed (decreases performance if set too low)
+        normalize=True, # normalize result
+        random_state=42 # seed for random generator
+    )
+
+    # initialize Coherence Model
+    cm = CoherenceModel(
+        model=nmf,
+        texts=texts,
+        dictionary=dictionary,
+        coherence='c_v'
+    )
+
+    coherence_scores.append(round(cm.get_coherence(), 5))
+
+# pick the # of topics with highest coherence score
+scores = list(zip(topic_nums, coherence_scores))
+best_num_topics = sorted(scores, key=itemgetter(1), reverse=True)[0][0]
+print('best_num_topics: {}'.format(best_num_topics))
+idx = int(best_num_topics/5)
+idx -= 1
+print('coherence_score: {}'.format(coherence_scores[idx]))
+
+# measure of word frequency in a document
+# adjusted by how many documents word appears in
+min_df = no_below
+max_df = no_above
+max_features = keep_n
+tfidf_vectorizer = TfidfVectorizer(
+    min_df=min_df,
+    max_df=max_df,
+    max_features=max_features,
+    ngram_range=(1,2),
+    preprocessor=' '.join
+)
+
+# returns document-term matrix (frequency of word i in document j)
+tfidf = tfidf_vectorizer.fit_transform(texts)
+# all the words we'll be looking at
+tfidf_fn = tfidf_vectorizer.get_feature_names()
+
+# learn a model
+# sklearn model
+nmf = NMF(
+    n_components=best_num_topics, 
+    init='nndsvd', # method for initialization procedure
+    max_iter=max_iter, # max number of iterations before timing out
+    l1_ratio=l1_ratio, # regularization mixing parameter (0 => l2 penalty, 1 => l1 penalty, (0,1) => mixture)
+    solver='cd', # coordinate descent solver
+    alpha=alpha, # constant that multiplies the regularization terms (set to 0 for no regularization)
+    tol=tol, # tolerance of the stopping condition
+    random_state=42 # seed for random generator
+).fit(tfidf)
+
+# transforms documents -> document-term matrix, transforms data according to model
+docweights = nmf.transform(tfidf_vectorizer.transform(texts)) # (articles x topics)
+
+# topic dataframe: (15, 8)
+# (topic num : top 8 words that describe the topic)
+n_top_words = 8
+topic_df = topic_table(
+    nmf,
+    tfidf_fn,
+    n_top_words
+).T
+
+# clean the topic words
+topic_df['topics'] = topic_df.apply(lambda x: [' '.join(x)], axis=1)
+topic_df['topics'] = topic_df['topics'].str[0]
+topic_df['topics'] = topic_df['topics'].apply(lambda x: whitespace_tokenizer(x))
+topic_df['topics'] = topic_df['topics'].apply(lambda x: unique_words(x))
+topic_df['topics'] = topic_df['topics'].apply(lambda x: [' '.join(x)])
+topic_df['topics'] = topic_df['topics'].str[0]
+
+# clean dataframe to include topic num and topic words as a list
+topic_df = topic_df['topics'].reset_index()
+topic_df.columns = ['topic_num', 'topics']
+
+topics = topic_df[['topic_num','topics']]
+topics.drop_duplicates()
+
+# merge original dataframe with topics
+url = df['url'].tolist()
+title = df['title'].tolist()
+text = df['text'].tolist()
+df_temp = pd.DataFrame({
+    'url':url,
+    'topic_num':docweights.argmax(axis=1)
+})
+merged_topic = df_temp.merge(
+    topic_df,
+    on='topic_num',
+    how='left'
+)
+complete_df = merged_topic.merge(
+    df,
+    on='url',
+    how='left'
+)
+
+complete_df = complete_df.drop('processed_text',axis=1)
+sorted_articles = complete_df.sort_values(by=['topic_num'])
+sorted_articles.to_csv(path+'sorted_articles.csv',header=True)
+
+# get num articles per topic
+num_articles_per_topic = []
+for topic in range(best_num_topics):
+    count = 0
+    for index, row in sorted_articles.iterrows():
+        if row['topic_num'] == topic:
+            count += 1
+    num_articles_per_topic.append(count)
+
+topics['num_articles'] = num_articles_per_topic
+
+# matrices from nmf (A = WH)
+mat_A = tfidf_vectorizer.transform(texts)
+mat_W = nmf.components_
+mat_H = nmf.transform(mat_A)
+# print('A = {} x {}'.format(mat_A.shape[0],mat_A.shape[1]))
+# print('W = {} x {}'.format(mat_W.shape[0],mat_W.shape[1]))
+# print('H = {} x {}'.format(mat_H.shape[0],mat_H.shape[1]))
+
+# residuals: measurement of how well the topics approximate the data (observed value - predicted value)
+# 0 -> topic perfectly predicts data
+# residual = Frobenius norm tf-idf weights (A) - coefficients of topics (H) X coefficients of topics (W)
+r = np.zeros(mat_A.shape[0]) # num articles
+for row in range(mat_A.shape[0]):
+    r[row] = np.linalg.norm(mat_A[row,:] - mat_H[row,:].dot(mat_W), 'fro')
+
+sum_sqrt_res = round(sum(np.sqrt(r)),3)
+print('Sum of the squared residuals is {}'.format(sum_sqrt_res))
+
+complete_df['resid'] = r
+complete_df.to_csv(path+'complete_df.csv',header=True)
+resid_data = complete_df[[
+    'topic_num','resid'
+]].groupby('topic_num').mean().sort_values(by='resid')
+complete_topics = resid_data.merge(
+    topics,
+    on='topic_num',
+    how='left'
+)
+complete_topics.to_csv(path+'topics.csv',index=True,header=True)
+
+# write parameters.txt
+parameters = open(test_path+'parameters.txt','w')
+parameters.write('coherence:\n')
+parameters.write('\nno_below: {}'.format(no_below))
+parameters.write('\nno_above: {}'.format(no_above))
+parameters.write('\nkeep_n: {}'.format(keep_n))
+parameters.write('\nchunksize: {}'.format(2000))
+parameters.write('\npasses: {}'.format(5))
+parameters.write('\nkappa: {}'.format(0.1))
+parameters.write('\nminimum_probability: {}'.format(0.01))
+parameters.write('\nw_max_iter: {}'.format(300))
+parameters.write('\nw_stop_condition: {}'.format(0.0001))
+parameters.write('\nh_max_iter: {}'.format(100))
+parameters.write('\nh_stop_condition: {}'.format(0.001))
+parameters.write('\neval_every: {}'.format(True))
+parameters.write('\n')
+parameters.write('\ntfidf_vectorizer:')
+parameters.write('\nn_components: {}'.format(best_num_topics))
+parameters.write('\nmin_df: {}'.format(min_df))
+parameters.write('\nmax_df: {}'.format(max_df))
+parameters.write('\nmax_features: {}'.format(max_features))
+parameters.write('\n')
+parameters.write('\nmodel:')
+parameters.write('\nmax_iter: {}'.format(500))
+parameters.write('\nl1_ratio: {}'.format(0.0))
+parameters.write('\nalpha: {}'.format(0.0))
+parameters.write('\ntol: {}'.format(1e-4))
+parameters.write('\n')
+parameters.write('\nsum_sqared_resids: {}'.format(sum_sqrt_res))
+parameters.write('\nbest_num_topics: {}'.format(best_num_topics))
+parameters.close()
