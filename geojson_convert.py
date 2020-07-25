5f24907252634c7d71869fbf4d66def9e11cd56b
Shayna <sgrose@sfu.ca>
2020-07-25 14:34:28 -0700

    cleaned up geojson conversion and added in comments

--- geojson_convert.py
+++ geojson_convert.py
@@ -5,66 +5,48 @@ import json
 from geojson import Feature, FeatureCollection, Point
 import requests
 from linkpreview import link_preview
+from collections import Counter
 
 
 data = pd.read_csv("final_df.csv")
 
+# drop articles with no location
 data = data[data["lat"] != 0]
 
-# print(data)
-
-new = data.copy()
+# NOTE: only uncomment this section if some of the latitudes and longitudes are mixed up
+# latitude should be positive, longitude should be negative (in most cases for BC locations)
+# new = data.copy()
 
 # replace latitudes that are negative and larger than longitudes with longitude
-data['lat'] = np.where(((data['lat'] < 0) & (
-    np.abs(data['lat']) > np.abs(data['long']))), data['long'], data['lat'])
+# data['lat'] = np.where(((data['lat'] < 0) & (
+#     np.abs(data['lat']) > np.abs(data['long']))), data['long'], data['lat'])
 
 
-data['long'] = np.where((data['long'] == data['lat']),
-                        new['lat'], data['long'])
+# data['long'] = np.where((data['long'] == data['lat']),
+#                         new['lat'], data['long'])
 
 
-data['local_score'] = np.random.randint(0, 100, size=len(data['title']))
-data['prov_score'] = np.random.randint(0, 100, size=len(data['title']))
-data['national_score'] = np.random.randint(0, 100, size=len(data['title']))
-data['inter_score'] = np.random.randint(
-    0, 100, size=len(data['title']))
+# NOTE: to assign random scores for relevancy comment out the following lines
+# data['local_score'] = np.random.randint(0, 100, size=len(data['title']))
+# data['prov_score'] = np.random.randint(0, 100, size=len(data['title']))
+# data['national_score'] = np.random.randint(0, 100, size=len(data['title']))
+# data['inter_score'] = np.random.randint(
+#     0, 100, size=len(data['title']))
 
 
-data = data[['url', 'title', 'topics', 'category', 'location', 'lat',
+# only keep the columns you want -> may require changing
+data = data[['url', 'title', 'topic', 'category', 'location', 'lat',
              'long', 'local_score', 'prov_score', 'national_score', 'inter_score']]
-# print(data)
 
-
-# url = 'https://api.linkpreview.net?key=8c19d38dbf3c23953a76008a7b4e7f74&q=' + \
-#     data['url']
-
-# url = 'https://api.linkpreview.net?key=8c19d38dbf3c23953a76008a7b4e7f74&q=https://google.com'
-# response = requests.request("POST", url)
-# # print(response.text.image)
-# print(response.text)
-data['image'] = np.nan
-
-
-def get_image(url):
-    image = "https://sisterhoodofstyle.com/wp-content/uploads/2018/02/no-image-1.jpg"
-    try:
-        preview = link_preview(url)
-        image = preview.image
-    except requests.exceptions.HTTPError:
-        pass
-    return image
-
-
-data['image'] = data['url'].apply(get_image)
-print(data['image'])
-
-data.to_csv("final_df_clean.csv", index=False, header=False)
+# header and index have to be false for the next part to work
+data.to_csv("test.csv", index=False, header=False)
 
 
 # convert into a proper geojson format for one article per object
+# the name of the csv you read has to be the same as the one you just wrote that has no index and header and the correct columns
+# the attributes in the for url, title, topic etc may require changing depending on which file you're reading
 features = []
-with open('final_df_clean.csv', newline='') as csvfile:
+with open('test.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
     for url, title, topic, category, location, latitude, longitude, local, prov, national, inter, image in reader:
         latitude, longitude = map(float, (latitude, longitude))
@@ -86,6 +68,7 @@ with open('final_df_clean.csv', newline='') as csvfile:
             )
         )
 
+# change X to indicate what you're grouping by
 collection = FeatureCollection(features)
-with open("articles.json", "w") as f:
+with open("grouped_by_X.json", "w") as f:
     f.write('%s' % collection)
