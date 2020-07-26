fc0c8bf2b917f998653168cb54059009d38c3d25
Miya Keilin <miya.keilin@gmail.com>
2020-07-25 20:01:45 -0700

    incorporate zoom scores into organize

--- news_project/organize.py
+++ news_project/organize.py
@@ -2,17 +2,9 @@ import pandas as pd
 import numpy as np
 import csv
 import json
-# from geojson import Feature, FeatureCollection, Point
 import requests
 from linkpreview import link_preview
-from collections import Counter
 
-data = pd.read_csv("final_df.csv")
-
-# add image column to data
-data['image'] = np.nan
-
-# use api to get image for each article, or use default if none exists
 def get_image(url):
     image = "https://sisterhoodofstyle.com/wp-content/uploads/2018/02/no-image-1.jpg"
     try:
@@ -25,7 +17,34 @@ def get_image(url):
 def truncate_list(lst):
     return lst[:5]
 
+data = pd.read_csv('groups/final_df.csv')
 # data['image'] = data['url'].apply(get_image)
+
+world_rank = pd.read_csv('groups/worldwide.csv')
+world_rank = world_rank.dropna()
+canada_rank = pd.read_csv('groups/canada.csv')
+canada_rank = canada_rank.dropna()
+bc_rank = pd.read_csv('groups/BC.csv')
+bc_rank = bc_rank.dropna()
+# print(world_rank.head())
+# print(canada_rank.head())
+# print(bc_rank.head())
+
+score_len = len(world_rank.index)
+world_rank = world_rank.iloc[0]
+world_rank = world_rank.T
+canada_rank = canada_rank.iloc[0]
+bc_rank = bc_rank.iloc[0]
+print(world_rank)
+print(canada_rank)
+print(bc_rank)
+
+# topics = data.topics.unique()
+# topic_list = []
+# for topic in topics:
+#     print(topic)
+#     topic = topic.split(' ')
+#     topic_list.append(topic[0])
 
 topic_nums = data.topic_num.unique()
 topic_groups = data.groupby('topic_num')
@@ -51,7 +70,23 @@ for num in topic_nums:
 
 topic_loc_df['titles'] = topic_loc_df['titles'].apply(truncate_list)
 topic_loc_df['urls'] = topic_loc_df['urls'].apply(truncate_list)
-# topic_loc_df['images_urls'] = topic_loc_df['image_urls'].apply(truncate_list)
+
+# print(topic_loc_df)
+
+world_scores = []
+canada_scores = []
+bc_scores = []
+for idx, row in topic_loc_df.iterrows():
+    topic = row['topic'].split(' ')[0]
+    col_name_world = topic + ': (Worldwide)'
+    col_name_canada = topic + ': (Canada)'
+    col_name_bc = topic + ': (British Columbia)'
+    world_scores.append(world_rank[col_name_world])
+    canada_scores.append(canada_rank[col_name_canada])
+    bc_scores.append(bc_rank[col_name_bc])
+topic_loc_df['world_score'] = world_scores
+topic_loc_df['canada_score'] = canada_scores
+topic_loc_df['bc_score'] = bc_scores
 
 print(topic_loc_df)
 
@@ -63,7 +98,7 @@ for idx,row in topic_loc_df.iterrows():
         image_list.append(get_image(url))
     urls.append(url_list)
 
-topic_loc_df['image_urls'] = urls
+# topic_loc_df['image_urls'] = urls
     
 
 topic_loc_df.to_csv('/Users/miya/Documents/GitHub/ai4good_news/news_project/topic_location.csv',index=False,header=True)
